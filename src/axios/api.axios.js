830cd6265b8981f83de18256601b0cceeea8e0f9
Prachi Kumari <prachi.kumari0004@gmail.com>
2026-02-28 16:54:24 +0530

    env update

--- src/axios/api.axios.js
+++ src/axios/api.axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
- baseURL: `${import.meta.env.VITE_API_URL}/api`,
+ baseURL:`${import.meta.env.VITE_API_URL}/api`,
   //  baseurl:"http://localhost:4000/api"    //this is when i did localhost in my browser
   // baseURL: "http://192.168.1.8:Port of backend/api",  //whene run on phone and desktop both 
 });
